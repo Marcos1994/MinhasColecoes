3b03c50cc2b16ddfacc6a5a9b590968f2ab33cc6
Marcos <marcos.antas@hotmail.com.br>
2022-01-07 20:36:15 -0300

    Adição das fotos nos itens e nas coleções de outros locais

--- MinhasColecoes/MinhasColecoes/MinhasColecoesAPI/ViewModels/ItemBasicViewModel.cs
+++ MinhasColecoes/MinhasColecoes/MinhasColecoesAPI/ViewModels/ItemBasicViewModel.cs
@@ -17,6 +17,7 @@ namespace MinhasColecoes.MinhasColecoesAPI.ViewModels
 		public ItemBasicViewModel ItemOriginal { get; set; }
 		public EnumRelacaoUsuarioItem Relacao { get; set; }
 		public string Comentario { get; set; }
+		public string Foto { get; set; }
 
 		public string CodigoNome
 		{ get { return ((Codigo != null && Codigo.Length > 0) ? $"{Codigo} - " : "") + Nome; } }
